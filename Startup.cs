69f678d540cd4039929dd8e91c074f694a3eb39c
Hongjiang Zhang <honzhan@microsoft.com>
2019-01-24 11:46:20 +0800

    update the AzureSignalR AspNet version

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,9 @@ namespace ChatRoom
         public void Configuration(IAppBuilder app)
         {
             // Any connection or hub wire up and configuration should go here
-            app.MapAzureSignalR(this.GetType().FullName);
+            app.MapAzureSignalR(this.GetType().FullName, options=> {
+                options.ConnectionCount = 10;
+            });
             //GlobalHost.TraceManager.Switch.Level = System.Diagnostics.SourceLevels.All;
         }
     }
